dadc71a4fc4a700a57afc051af1798588d4695f3
Sushant Singh <sushantgkp9450@gmail.com>
2026-07-29 09:59:11 +0530

    Added OOPS programs

---
+++ Passbyreference.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int Sum(int*,int*);
+// int Sum(int *x,int*y){
+//     int S;
+//     S=*x+*y;
+//     return S;
+// }
+int main() {
+    int a,b,r;
+    cout<<"Enter the value of a and b:"<<endl;
+    cin>>a>>b;
+    r=Sum(&a,&b);
+    cout<<r<<endl;
+}
+int Sum(int *x,int*y){
+    int S;
+    S=*x+*y;
+    return S;
+}
